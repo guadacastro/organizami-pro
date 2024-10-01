b409e7b672520fa4d554ef80248ae35f6a2ecd11
guadacastro <guadycasmar123@gmail.com>
2024-10-01 16:54:06 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './Header.css'; // Optional: For styling
+import '../styles/Header.css';
 
 function Header() {
   return (
     <header>
-      <h1>To-Do List</h1>
+        
+      <h1>This is the Header</h1>
       <p>Your tasks organized</p>
     </header>
   );
