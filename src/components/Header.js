039b40d0a00f96f65baa64f18695e7791c5144fc
guadacastro <guadycasmar123@gmail.com>
2024-10-08 14:05:32 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,19 @@ import '../styles/Header.css';
 function Header() {
   return (
     <header>
-      <div className="box flex justify-between">
+      <div className="container flex justify-between items-center bg-red-600x">
         <img src={require('../assets/fox.png')} alt="Fox" className='logo' />
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+        <nav className="flex justify-center gap-5">
+          <a>Home</a>
+          <a>To do list</a>
+          <a>About Us</a>
+          <a>Contact</a>
+        </nav>
+        <ul className="flex flex-row gap-5">
+          <li>Login</li>
+          <li>Sign Up</li>
         </ul>
-        <div>login</div>
+      
       </div>
     </header>
   );
