d20aa0b5fe02749356a7aacac10ae8410f72f30a
guadacastro <guadycasmar123@gmail.com>
2024-10-08 13:50:50 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,15 @@ import '../styles/Header.css';
 function Header() {
   return (
     <header>
-      <div className="box">
-        <div>Logo</div>
-        <div>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
+      <div className="box flex justify-between">
+        <img src={require('../assets/fox.png')} alt="Fox" className='logo' />
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
         <div>login</div>
       </div>
-      
     </header>
   );
 }
